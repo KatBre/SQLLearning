fe0924948c93b2ddda977c144c9e9ed7530200aa
Katarzyna Brewinska <brewinska.katarzyna@gmail.com>
2020-08-17 22:34:31 +0200

    ex5 added commit

--- exercise5.sql
+++ exercise5.sql
@@ -46,3 +46,31 @@ INSERT INTO Provides(Piece, Provider, Price) VALUES(3,'RBT',50);
 INSERT INTO Provides(Piece, Provider, Price) VALUES(3,'TNBC',45);
 INSERT INTO Provides(Piece, Provider, Price) VALUES(4,'HAL',5);
 INSERT INTO Provides(Piece, Provider, Price) VALUES(4,'RBT',7);
+
+-- 5.1 Select the name of all the pieces. 
+SELECT Name FRoM Pieces;
+-- 5.2  Select all the providers' data. 
+SELECT * FROM Providers;
+-- 5.3 Obtain the average price of each piece (show only the piece code and the average price).
+SELECT p.Code, AVG(prvds.Price) FROM Pieces p JOIN Provides prvds ON p.Code=prvds.Piece GROUP BY p.Code;
+-- 5.4  Obtain the names of all providers who supply piece 1.
+SELECT prvdrs.Name FROM Provides ;
+-- 5.5 Select the name of pieces provided by provider with code "HAL".
+
+-- 5.6
+-- ---------------------------------------------
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- Interesting and important one.
+-- For each piece, find the most expensive offering of that piece and include the piece name, provider name, and price 
+-- (note that there could be two providers who supply the same piece at the most expensive price).
+-- ---------------------------------------------
+
+-- 5.7 Add an entry to the database to indicate that "Skellington Supplies" (code "TNBC") will provide sprockets (code "1") for 7 cents each.
+
+-- 5.8 Increase all prices by one cent.
+UPDATE Provides SET Price = Price + 1;
+-- 5.9 Update the database to reflect that "Susan Calvin Corp." (code "RBT") will not supply bolts (code 4).
+
+-- 5.10 Update the database to reflect that "Susan Calvin Corp." (code "RBT") will not supply any pieces 
+    -- (the provider should still remain in the database).
+    
